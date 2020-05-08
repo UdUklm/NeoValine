a5fdaf02ea6d6d25238d2aa7e087355bfdf742ca
UdUklm <1491989333@qq.com>
2020-05-08 12:09:13 +0800

    rm test file

--- src/test.js
+++
@@ -1,6 +0,0 @@
-var comment = {}
-comment['link'] = 'https://www.ohmysites.com'
-
-comment['link'] = comment['link']?(/^https?\:\/\//.test(comment['link']) ? comment['link'] : 'http://'+comment['link']) : '';
-
-console.log(comment['link'])
